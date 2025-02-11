2c2710908ff0e70d3ef1c98550d565db6ea79c2f
Maxim Petrishin <webmaksyao@gmail.com>
2025-02-11 21:31:50 +0300

    Initial commit

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+-- files
+vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "E", "$", { noremap = false })
+vim.api.nvim_set_keymap("n", "B", "^", { noremap = false })
+vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", { noremap = true })
